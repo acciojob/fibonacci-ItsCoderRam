269cc208e38168bc05ddb611a6b0e0379ca2241c
ItsCoderRam <54639693+ItsCoderRam@users.noreply.github.com>
2024-09-02 21:45:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function fibonacci(num) {
 	let a = 0;
   let b = 1;
 
-  if (num === 0) {
+  if (num == 0) {
     return a;
   }
- if (num === 1) {
+ if (num == 1) {
     return 1;
   }
   for (let i = 2; i <= num; i++) {
